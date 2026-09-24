fe548689b3627f8e4411218f59657ac0d0b4b013
ishan <ishanrajvi2001@gmail.com>
2026-09-23 20:26:02 -0500

    understood setting up of a bell state and measurements

--- quantum/Bell_state/Bell_State.py
+++ quantum/Bell_state/Bell_State.py
@@ -1,5 +1,27 @@
 from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
 
 circuit = QuantumCircuit(2)
 
+
+
+# apply hadmard to qubit 0
+circuit.h(0)
+
+# entagle qubit 0 to 1
+circuit.cx(0, 1)
+
+# measurment
+circuit.measure_all()
+
 print(circuit)
+
+sampler = StatevectorSampler()
+
+job = sampler.run([circuit], shots = 1000)
+results = job.result()
+
+count = results[0].data.meas.get_counts()
+
+print(count)
+
